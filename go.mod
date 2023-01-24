a6eaf12bf158f1313143f759eeb96b38ff56f2a0
Simon Striekwold <simon.striekwold@joule-technologies.com>
2023-01-24 09:15:22 +0100

    add non working unittest with ramsql

--- go.mod
+++ go.mod
@@ -47,6 +47,7 @@ require (
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/proullon/ramsql v0.0.0-20220719091513-bf3c20043516 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
@@ -56,4 +57,5 @@ require (
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gotest.tools v2.2.0+incompatible // indirect
 )
--- src/handlers_test.go
+++ src/handlers_test.go
@@ -3,9 +3,13 @@ package src_test
 import (
 	"database/sql"
 	"fmt"
+	"settingsstore/gen/models"
+	"settingsstore/gen/restapi/operations"
 	"settingsstore/src"
 	"testing"
+	"time"
 
+	_ "github.com/proullon/ramsql/driver"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -26,27 +30,27 @@ func (suite *ExampleTestSuite) SetupTest() {
 	fmt.Println(err)
 	db, err := gorm.Open(postgres.New(postgres.Config{Conn: sqlDB}), &gorm.Config{})
 	fmt.Println(err)
-	// src.Migrate(db)
-	// todo: close
-	_ = db
+	err = src.Migrate(db)
+	fmt.Println(err)
+	// todo: close?
 }
 
 func (suite *ExampleTestSuite) TestExample() {
-	// expectedSetting := src.Setting{
-	// 	ID:    42,
-	// 	Type:  "default",
-	// 	Value: "some value",
-	// 	End:   time.Now(),
-	// }
-	// inputSetting := models.Setting{
-	// 	ID:    &expectedSetting.ID,
-	// 	Type:  &expectedSetting.Type,
-	// 	Value: &expectedSetting.Value,
-	// 	End:   &models.DateTime{Time: expectedSetting.End},
-	// }
+	expectedSetting := src.Setting{
+		ID:    42,
+		Type:  "default",
+		Value: "some value",
+		End:   time.Now(),
+	}
+	inputSetting := models.Setting{
+		ID:    &expectedSetting.ID,
+		Type:  &expectedSetting.Type,
+		Value: &expectedSetting.Value,
+		End:   &models.DateTime{Time: expectedSetting.End},
+	}
 
 	// _, _ = expectedSetting, inputSetting
-	// _ = suite.handler.AddSetting(operations.AddSettingParams{Setting: &inputSetting}, nil)
+	_ = suite.handler.AddSetting(operations.AddSettingParams{Setting: &inputSetting}, nil)
 
 	// assert.Equal(suite.T(), operations.NewAddSettingCreated(), result)
 	// assert.Equal(suite.T(), expectedSetting, suite.SettingInDB())
